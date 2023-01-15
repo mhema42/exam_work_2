3273cebd17d8b1b013eb0864245f14bbfd7c71fa
Mats <mhema@telia.com>
2023-01-15 22:22:55 +0100

    first commit add backend BP

---
+++ src/test/java/com/example/xwork/XworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
