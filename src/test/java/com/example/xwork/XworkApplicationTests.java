e87f7d01b3e0f6cbc93774954f33a186a5507bf8
Mats <mhema@telia.com>
2023-01-21 23:01:35 +0100

    minor fix, removed unused test

--- src/test/java/com/example/xwork/XworkApplicationTests.java
+++ src/test/java/com/example/xwork/XworkApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.xwork;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XworkApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
