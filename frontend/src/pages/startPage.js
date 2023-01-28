c3595d9b24877168c64bd59aa2fce6eed4c56a3e
Mats <mhema@telia.com>
2023-01-28 20:29:16 +0100

    add purchase modal

--- frontend/src/pages/startPage.js
+++ frontend/src/pages/startPage.js
@@ -3,18 +3,23 @@ import "../css/startPage.css";
 
 const StartPage = () => {
     const [products, setProducts] = useState([])
-    const [purchaseId, setPurchaseId] = useState("");
+    const [product, setProduct] = useState([])
     const [productId, setProductId] = useState("");
+    const [purchaseId, setPurchaseId] = useState("");
     const [quantity, setQuantity] = useState("");
-    const [message, setMessage] = useState("");
 
-    const [style, setStyle] = useState("show");
+    const [purchaseModal, setModal] = useState("hide");
+    const [dimProducts, setDimProducts] = useState("no_dim");
 
-    const changeStyle = () => {
-        if (style === "hide") {
-         setStyle("show")
-        } else {setStyle("hide")}
-    };
+    const toggleModal = () => {
+        if (purchaseModal === "show" || dimProducts === "dim") {
+         setModal("hide")
+         setDimProducts("no_dim")
+        } else {
+            setModal("show")
+            setDimProducts("dim")
+            }
+    };  
 
     useEffect(() => {
         fetch("http://localhost:8080/product", {
@@ -29,9 +34,25 @@ const StartPage = () => {
             })
     }, [])
 
-    const handleSubmit = (e) => {
+    const handleSubmit1 = (e) => {
         e.preventDefault()
-        fetch(`http://localhost:8080/purchase?purchaseId=${purchaseId}&productId=${productId}&quantity=${quantity}`, {
+        
+        fetch("http://localhost:8080/product/" + productId, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then(res => res.json())
+            .then((result) => {
+                setProduct(result);
+            })
+    }
+
+    const handleSubmit2 = (e) => {
+        e.preventDefault()
+
+        fetch(`http://localhost:8080/purchase?purchaseId=${purchaseId}&productId=${product}&quantity=${quantity}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -43,42 +64,70 @@ const StartPage = () => {
                 });
             }
         });
-        setMessage("product added to chopping cart")
+    }
+
+    const reset = () => {
+        setPurchaseId("");
+        setQuantity("");
     }
 
     return (
         <div>
-            <div className="Page-container">
-                <h1>All products</h1>
+            <div className="page-container">
 
-                {products.map(product => (           
-                    <div key={product.id}>
-                        <span> Product: {product.name} </span> 
-                        <span> Price: {product.price} </span>
+                <div className={dimProducts}></div>
 
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                value={purchaseId}
-                                placeholder="purchaseId"
-                                onChange={(e) => setPurchaseId(e.target.value)}
-                            /><br />
-                            <input
-                                value={quantity}
-                                placeholder="quantity"
-                                onChange={(e) => setQuantity(e.target.value)}
-                            /><br />
-                            <button 
-                                value={product.id}
+                <div className="all_products">
+                    <h1>All products</h1>
+                    {products.map(p => (           
+                        <div key={p.id}>
+                            <span> Product: {p.name} </span><br />
+                            <span> Price: {p.price} </span>
+                        <form onSubmit={handleSubmit1}>
+                            <button className="buy-button"
+                                value={p.id}
                                 type="submit"
-                                onClick={(e) => setProductId(e.target.value)}>
-                                Submitt purchase
+                                onClick={(e) => {reset(); setProductId(e.target.value); toggleModal()}}>
+                                Cart
                             </button>
                         </form><br />
-                                       
-                    </div>           
-                ))}        
-            </div>
+                        </div>
+                    ))}
+                </div>
+               
+                <div className={purchaseModal}>
+                    <span> Product: {product.name} </span><br />
+                    <span> Price: {product.price} </span>
 
+                    <form onSubmit={handleSubmit2}>
+                        <input
+                            value={purchaseId}
+                            placeholder="purchaseId"
+                            onChange={(e) => setPurchaseId(e.target.value)}
+                        /><br />
+                        <input
+                            required
+                            value={quantity}
+                            placeholder="quantity"
+                            onChange={(e) => setQuantity(e.target.value)}
+                        /><br />
+                        <button className="buy-button"
+                            disabled={!quantity > 0}
+                            value={productId}
+                            type="submit"
+                            onClick={(e) => {setProduct(e.target.value); toggleModal()}}>
+                            Buy
+                        </button>
+                    </form><br />
+
+                    <button 
+                        className="close"
+                        type="close"
+                        onClick={() => toggleModal()}>
+                        X
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
